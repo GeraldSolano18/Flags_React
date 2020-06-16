94d51d82b4b45336462f1418114a76a2cce88682
Your Name <Gerald Solano>
2020-06-16 00:56:29 -0600

    primer commit de flags

--- src/App.js
+++ src/App.js
@@ -16,7 +16,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reactmamala
+
         </a>
       </header>
     </div>
